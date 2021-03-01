c92bd2588f95badbf626f657ddf3139dbea57c91
Christian <christiansj10@gmail.com>
2021-03-01 14:44:16 -0600

    STEntry contructor tests

--- src/test/mysql_interpreter/stentry/STEntryTest.java
+++ src/test/mysql_interpreter/stentry/STEntryTest.java
@@ -1,4 +1,21 @@
 package test.mysql_interpreter.stentry;
 
+import mysql_interpreter.classification.Classif;
+import mysql_interpreter.classification.Subclassif;
+import mysql_interpreter.stentry.STEntry;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class STEntryTest {
+    @Test
+    @DisplayName("constructor should properly set class variables")
+    public void testConstructor(){
+        STEntry stEntry = new STEntry("mySymbol", Classif.ARTICLE, Subclassif.BUILTIN);
+        
+        assertEquals("mySymbol", stEntry.symbol);
+        assertEquals(Classif.ARTICLE, stEntry.primClassif);
+        assertEquals(Subclassif.BUILTIN, stEntry.subclassif);
+    }
 }
