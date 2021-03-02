92a32514c296e6a935e303bfa07e7cc772dae1a6
Christian <christiansj10@gmail.com>
2021-03-02 15:32:10 -0600

    implementing MySQLInterpreter in main method

--- src/mysql_interpreter/MySQLInterpreter.java
+++ src/mysql_interpreter/MySQLInterpreter.java
@@ -1,7 +1,39 @@
 package mysql_interpreter;
 
+import mysql_interpreter.scanner.MySQLScanner;
+import mysql_interpreter.symbol_table.SymbolTable;
+
 public class MySQLInterpreter {
+    private static void runScanner(String filePath) throws Exception{
+        MySQLScanner scanner = new MySQLScanner(filePath, new SymbolTable());
+
+        scanner.printCurrentLine();
+        printTokenHeading();
+        int currentLine = 1;
+
+        while(!scanner.getNext().isEmpty()){
+            scanner.currentToken.printToken();
+
+            if(currentLine < scanner.getLineNumber() && scanner.getLineNumber() != scanner.getLineList().size()){
+                scanner.printCurrentLine();
+                printTokenHeading();
+                currentLine += 1;
+            }
+        }
+    }
+
+    private static void printTokenHeading(){
+        System.out.printf("%-11s %-12s %s\n"
+                , "primClassif"
+                , "subClassif"
+                , "tokenStr");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        try{
+            runScanner(args[0]);
+        }catch(Exception e){
+            System.err.println(e);
+        }
     }
 }
