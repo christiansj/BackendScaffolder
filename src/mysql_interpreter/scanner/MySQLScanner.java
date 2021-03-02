cf795ec33f05562b674f0b18b596d0ca8989c564
Christian <christiansj10@gmail.com>
2021-03-02 15:08:43 -0600

    MySQLScanner class

---
+++ src/mysql_interpreter/scanner/MySQLScanner.java
@@ -0,0 +1,162 @@
+package mysql_interpreter.scanner;
+
+import mysql_interpreter.classification.Classif;
+import mysql_interpreter.classification.Subclassif;
+import mysql_interpreter.stentry.STEntry;
+import mysql_interpreter.symbol_table.SymbolTable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class MySQLScanner {
+    private final String DELIMITERS = " ();,\n\t";
+    private SymbolTable symbolTable;
+
+    private int iColPos;
+    private int iLineNumber;
+    private String inputFileName;
+    private ArrayList<String> lineList = new ArrayList<>();
+    private char[] textCharM;
+
+    public Token currentToken = new Token();
+    public Token nextToken;
+
+    public MySQLScanner(String fileName, SymbolTable symbolTable) throws Exception{
+        this.inputFileName = fileName;
+        this.symbolTable = symbolTable;
+        iColPos = 0;
+        iLineNumber = 0;
+
+        initLineList();
+        if(lineList.size() > 0){
+            this.textCharM = lineList.get(0).toCharArray();
+        }
+
+        nextToken = readToken();
+    }
+
+    private void initLineList() throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(this.inputFileName));
+        String line;
+
+        while((line = reader.readLine()) != null){
+            this.lineList.add(line);
+        }
+    }
+
+    private Token readToken(){
+        String line;
+
+        while(iLineNumber < lineList.size()){
+            line = lineList.get(iLineNumber);
+            textCharM = line.toCharArray();
+
+            if(iColPos < textCharM.length){
+                return createToken(line);
+            }
+
+            iLineNumber += 1;
+            iColPos = 0;
+        }
+
+        return new Token();
+    }
+
+    private Token createToken(String line) {
+        Token token = new Token();
+        eatWhiteSpace();
+
+        token.iColPos = iColPos;
+        token.iLineNum = iLineNumber;
+        String firstChar = String.valueOf(textCharM[token.iColPos]);
+
+        if("();".contains(firstChar)){
+            token.tokenStr = firstChar;
+            iColPos += 1;
+        }else{
+            token.tokenStr = line.substring(token.iColPos, findEndOfToken());
+        }
+
+        return token;
+    }
+
+    private void eatWhiteSpace(){
+        while(iColPos < textCharM.length){
+            String currentChar = String.valueOf(textCharM[iColPos]);
+            if(!" \t\n".contains(currentChar)){
+                return;
+            }
+            iColPos+=1;
+        }
+    }
+
+    private int findEndOfToken(){
+        while(iColPos < textCharM.length){
+            if(DELIMITERS.contains((String.valueOf(textCharM[iColPos])))){
+                return iColPos;
+            }
+            iColPos += 1;
+        }
+        return iColPos;
+    }
+
+    public String getNext(){
+        // the last line has been scanned
+        // return an EOF token
+        if(iLineNumber == lineList.size()) {
+            currentToken.tokenStr = "";
+            currentToken.primClassif = Classif.EOF;
+            currentToken.subclassif = Subclassif.EMPTY;
+
+            return "";
+        }
+
+        currentToken = nextToken;
+
+        setTokenClass();
+        readNextToken();
+
+        return currentToken.tokenStr;
+    }
+
+    private void readNextToken(){
+        Token temp = currentToken;
+
+        nextToken = readToken();
+        currentToken = nextToken;
+        if(!currentToken.tokenStr.equals("")){
+            setTokenClass();
+        }
+
+        currentToken = temp;
+    }
+
+    private void setTokenClass(){
+        String tokenStr = currentToken.tokenStr;
+        STEntry stEntry = symbolTable.getSTEntry(tokenStr);
+
+        if(stEntry != null){
+            currentToken.primClassif = stEntry.primClassif;
+            currentToken.subclassif = stEntry.subclassif;
+        }else if("(),;".contains(tokenStr)){
+            currentToken.primClassif = Classif.SEPARATOR;
+        }else if(String.valueOf(tokenStr.charAt(0)).matches("\\d")){
+            currentToken.subclassif = Subclassif.INTEGER;
+        }else{
+            currentToken.subclassif = Subclassif.IDENTIFIER;
+        }
+    }
+
+    public void printCurrentLine(){
+        System.out.printf(" %d. %s\n", iLineNumber+1, lineList.get(iLineNumber));
+    }
+
+    public int getLineNumber(){
+        return iLineNumber;
+    }
+
+    public ArrayList<String> getLineList(){
+        return lineList;
+    }
+}
---
+++ src/test/mysql_interpreter/scanner/MySqlScannerTest.java
@@ -0,0 +1,143 @@
+package test.mysql_interpreter.scanner;
+
+import mysql_interpreter.classification.Classif;
+import mysql_interpreter.classification.Subclassif;
+import mysql_interpreter.scanner.MySQLScanner;
+import mysql_interpreter.scanner.Token;
+import mysql_interpreter.symbol_table.SymbolTable;
+
+import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MySqlScannerTest {
+    private final String INPUT_FOLDER_PATH = "src/test/mysql_interpreter/scanner/test_input/";
+
+    private MySQLScanner newTestScanner(String fileName) throws Exception{
+        return new MySQLScanner(INPUT_FOLDER_PATH + fileName, new SymbolTable());
+    }
+
+    @Test
+    @DisplayName("Constructor should properly initialize member variables")
+    public void testConstructor() throws Exception{
+        MySQLScanner scanner = newTestScanner("test1.txt");
+        final Token currentToken = scanner.currentToken;
+        final Token nextToken = scanner.nextToken;
+
+        assertEquals("CREATE TABLE MyTest( 1", scanner.getLineList().get(0));
+
+        assertEquals("", currentToken.tokenStr);
+        assertEquals(Classif.EMPTY, currentToken.primClassif);
+        assertEquals(Subclassif.EMPTY, currentToken.subclassif);
+
+        assertEquals("CREATE", nextToken.tokenStr);
+        assertEquals(Classif.EMPTY, nextToken.primClassif);
+        assertEquals(Subclassif.EMPTY, nextToken.subclassif);
+    }
+
+    @Test
+    @DisplayName("Constructor with nonexistent file should throw FileNotFoundException")
+    public void testConstructorWithNonexistentFile(){
+        assertThrows(FileNotFoundException.class, ()->{
+            newTestScanner("BADINPUT.txt");
+        });
+    }
+
+    @Test
+    @DisplayName("getNext method should return tokenStr of the next token")
+    public void testGetNextReturnValue() throws Exception{
+        MySQLScanner scanner = newTestScanner("test1.txt");
+        assertEquals("CREATE", scanner.getNext());
+    }
+
+    @Test
+    @DisplayName("getNext method should set member variables of currentToken")
+    public void testGetNextCurrentToken() throws Exception{
+        MySQLScanner scanner = newTestScanner("test1.txt");
+        scanner.getNext();
+
+        assertEquals("CREATE", scanner.currentToken.tokenStr);
+        assertEquals(Classif.OPERATION, scanner.currentToken.primClassif);
+        assertEquals(Subclassif.BUILTIN, scanner.currentToken.subclassif);
+    }
+
+    @Test
+    @DisplayName("getNext method should set member variables of nextToken")
+    public void testGetNext_NextToken() throws Exception{
+        MySQLScanner scanner = newTestScanner("test1.txt");
+        scanner.getNext();
+
+        assertEquals("TABLE", scanner.nextToken.tokenStr);
+        assertEquals(Classif.ARTICLE, scanner.nextToken.primClassif);
+        assertEquals(Subclassif.BUILTIN, scanner.nextToken.subclassif);
+    }
+
+    @Test
+    @DisplayName("scanning in EOF token at end of file")
+    public void testEOFToken() throws Exception {
+        MySQLScanner scanner = newTestScanner("test1.txt");
+        String tokenStr = "A";
+
+        while(!tokenStr.isEmpty()){
+            tokenStr = scanner.getNext();
+        }
+
+        assertEquals("", tokenStr);
+        assertEquals("", scanner.currentToken.tokenStr);
+        assertEquals(Classif.EOF, scanner.currentToken.primClassif);
+        assertEquals(Subclassif.EMPTY, scanner.currentToken.subclassif);
+    }
+
+    private void fileTokenClassTests(String fileName, Classif expectedClassif, Subclassif expectedSubclassif) throws Exception{
+        MySQLScanner scanner = newTestScanner(fileName);
+
+        while(!scanner.getNext().isEmpty()){
+            assertEquals(expectedClassif, scanner.currentToken.primClassif);
+            assertEquals(expectedSubclassif, scanner.currentToken.subclassif);
+        }
+    }
+    @Test
+    @DisplayName("scanning in SEPARATOR tokens")
+    public void testSeparatorTokens() throws Exception{
+        fileTokenClassTests("separators.txt", Classif.SEPARATOR, Subclassif.EMPTY);
+    }
+
+    @Test
+    @DisplayName("scanning in INTEGER tokens")
+    public void testNumericalTokens() throws Exception{
+        fileTokenClassTests("numericals.txt", Classif.EMPTY, Subclassif.INTEGER);
+    }
+
+    @Test
+    @DisplayName("scanning in IDENTIFIER tokens")
+    public void testIdentifierTokens() throws Exception{
+        fileTokenClassTests("indentifiers.txt", Classif.EMPTY, Subclassif.IDENTIFIER);
+    }
+
+    @Test
+    @DisplayName("scanning in OPERATION tokens")
+    public void testOperationTokens() throws Exception{
+        fileTokenClassTests("operations.txt", Classif.OPERATION, Subclassif.BUILTIN);
+    }
+
+    @Test
+    @DisplayName("scanning in KEY_TYPE tokens")
+    public void testKeyTypeTokens() throws Exception{
+        fileTokenClassTests("key_types.txt", Classif.KEY_TYPE, Subclassif.BUILTIN);
+    }
+
+    @Test
+    @DisplayName("scanning in ARTICLE tokens")
+    public void testArticleTokens() throws Exception{
+        fileTokenClassTests("articles.txt", Classif.ARTICLE, Subclassif.BUILTIN);
+    }
+
+    @Test
+    @DisplayName("scanning in DEFINE (MySQL type) tokens")
+    public void testDefineTokens() throws Exception{
+        fileTokenClassTests("mysql_types.txt", Classif.CONTROL, Subclassif.DECLARE);
+    }
+}
