de3112596c6da9cbf1daff2fb8df1fec641bc26c
Christian <christiansj10@gmail.com>
2021-03-31 14:10:13 -0500

    formatMySQLVariable utility function

--- src/springwriter/SpringWriterUtil.java
+++ src/springwriter/SpringWriterUtil.java
@@ -5,7 +5,22 @@ public class SpringWriterUtil {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n");
 		for(String s : packages) {
-			sb.append(String.format("import %s.%s\n", rootString, s));
+			sb.append(String.format("import %s.%s;\n", rootString, s));
+		}
+		
+		return sb.toString();
+	}
+	
+	public static String formatMySQLVariable(String mysqlVar) {
+		final String[] TOKENS = mysqlVar.split("_");
+		StringBuilder sb = new StringBuilder(TOKENS[0]);
+		
+		for(int i = 1; i < TOKENS.length; i++){
+		    String t = TOKENS[i];
+			sb.append(String.format("%c%s",
+				Character.toUpperCase(t.charAt(0)), 
+		        t.substring(1, t.length())
+		    ));
 		}
 		
 		return sb.toString();
--- src/test/springwriter/SpringWriterUtilTest.java
+++ src/test/springwriter/SpringWriterUtilTest.java
@@ -12,8 +12,15 @@ public class SpringWriterUtilTest {
 	@Test
 	@DisplayName("writeImports should return String with imports")
 	public void writeImportsTest() {
-		String expected = "\nimport com.alpha.bravo.one\nimport com.alpha.bravo.two\nimport com.alpha.bravo.three\n";
+		String expected = "\nimport com.alpha.bravo.one;\nimport com.alpha.bravo.two;\nimport com.alpha.bravo.three;\n";
 		String[] packages = {"one", "two", "three"};
 		assertEquals(expected, SpringWriterUtil.writeImports("com.alpha.bravo", packages));
+	}
+	
+	@Test
+	@DisplayName("formatMySQLVariable should return camelcased String with removed underscores")
+	public void formatMySQLVariableTest() {
+		assertEquals("one", SpringWriterUtil.formatMySQLVariable("one"));
+		assertEquals("alphaBravoCharlie", SpringWriterUtil.formatMySQLVariable("alpha_bravo_charlie"));
 	}
 }
