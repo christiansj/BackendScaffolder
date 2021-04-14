3b39cf63750b26851678c1bfde516717172c7eec
Christian <christiansj10@gmail.com>
2021-04-14 12:11:06 -0500

    adding ResponseEntity and HttpStatus to Controller

--- src/springwriter/controller/SpringControllerWriter.java
+++ src/springwriter/controller/SpringControllerWriter.java
@@ -38,7 +38,7 @@ public class SpringControllerWriter extends SpringFileWriter implements SpringFi
 		PrintWriter pw = new PrintWriter(sw);
 
 		final String[] ANNOTATIONS = {"RestController", "GetMapping", "PostMapping", "RequestBody", "PathVariable"};
-		
+		final String[] HTTPS = {"ResponseEntity", "HttpStatus"};
 		pw.println("\n");
 		pw.println("import java.util.List;");
 		pw.println(String.format("import %s.%s;", 
@@ -49,6 +49,7 @@ public class SpringControllerWriter extends SpringFileWriter implements SpringFi
 				TABLE_NAME));
 		pw.println("import org.springframework.beans.factory.annotation.Autowired;\n");
 		
+		pw.println(SpringWriterUtil.writeImports("org.springframework.http", HTTPS));
 		pw.println(SpringWriterUtil.writeImports("org.springframework.web.bind.annotation", ANNOTATIONS));
 //		pw.println("\n");
 		
@@ -73,34 +74,57 @@ public class SpringControllerWriter extends SpringFileWriter implements SpringFi
 	
 	private void writeGetAll(PrintWriter pw) {
 		pw.println(mappingStr("Get", MODEL_PATH));
-		pw.println(String.format("\tList<%s> getAll(){", TABLE_NAME));
-		pw.println("\t\treturn repository.findAll();");
+		pw.println(String.format("\tResponseEntity<List<%s>> getAll(){", TABLE_NAME));
+		pw.println("\t\treturn ResponseEntity.ok(repository.findAll());");
 		pw.println("\t}\n");
 	}
 	
 	private void writeGetOne(PrintWriter pw) throws Exception {
 		pw.println(mappingStr("Get", MODEL_PATH + "/{id}"));
 		pw.println(String.format("\t%s getOne(@PathVariable %s id){",
-				TABLE_NAME, PRIMARY_TYPE
+				entityStr(), PRIMARY_TYPE
 		));
-		pw.println("\t\treturn repository.findById(id);");
+		pw.println(String.format("\t\t%s %s = repository.findById(id);", 
+				TABLE_NAME, MODEL_PATH
+		));
+		pw.println(ifNotFoundStr(false));
+		
+		pw.println(String.format("\t\treturn ResponseEntity.ok(%s);", MODEL_PATH));
 		pw.println("\t}\n");
+	}
+	
+	private String ifNotFoundStr(boolean isFindInRepo) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(String.format("\t\tif(%s == null){\n", 
+				isFindInRepo ? "repository.findById(id)" : MODEL_PATH
+		));
+		sb.append(String.format("\t\t\treturn new %s(HttpStatus.NOT_FOUND);\n", entityStr()));
+		sb.append("\t\t}\n");
+		
+		return sb.toString();
+	}
+	
+	private String entityStr() {
+		return String.format("ResponseEntity<%s>", TABLE_NAME);
 	}
 	
 	private void writePost(PrintWriter pw) {
 		pw.println(mappingStr("Post", MODEL_PATH));
 		pw.println(String.format("\t%s post%s(@RequestBody %s new%s){", 
-				TABLE_NAME, TABLE_NAME, TABLE_NAME, TABLE_NAME
+				entityStr(), TABLE_NAME, TABLE_NAME, TABLE_NAME
 		));
-		pw.println(String.format("\t\treturn repository.save(new%s);", TABLE_NAME));
+		pw.println(String.format("\t\treturn ResponseEntity.ok(repository.save(new%s));", 
+				TABLE_NAME));
 		pw.println("\t}\n");
 	}
 	
 	private void writePut(PrintWriter pw) {
 		pw.println(mappingStr("Put", MODEL_PATH + "/{id}"));
 		pw.println(String.format("\t%s put%s(@RequestBody %s new%s, @PathVariable %s id){", 
-				TABLE_NAME, TABLE_NAME, TABLE_NAME, TABLE_NAME, PRIMARY_TYPE
+				entityStr(), TABLE_NAME, TABLE_NAME, TABLE_NAME, PRIMARY_TYPE
 		));
+		pw.println(ifNotFoundStr(true));
 		
 		final String PRIMARY_NAME = mySQLTable.getPrimaryKeyNames().get(0);
 		pw.println(String.format("\t\tnew%s.set%s(id);", 
@@ -108,16 +132,19 @@ public class SpringControllerWriter extends SpringFileWriter implements SpringFi
 				SpringWriterUtil.uppercaseFirstChar(PRIMARY_NAME)
 		));
 		
-		pw.println(String.format("\t\treturn repository.save(new%s);", TABLE_NAME));
+		pw.println(String.format("\t\treturn ResponseEntity.ok(repository.save(new%s));", TABLE_NAME));
 		pw.println("\t}\n");
 	}
 	
 	private void writeDelete(PrintWriter pw) {
 		pw.println(mappingStr("Delete", MODEL_PATH + "/{id}"));
-		pw.println(String.format("\tvoid delete%s(@PathVariable %s id){", 
-				TABLE_NAME, PRIMARY_TYPE
-				));
-		pw.println("\t\trepository.deleteById(id);");
+		pw.println(String.format("\t%s delete%s(@PathVariable %s id){", 
+				entityStr(), TABLE_NAME, PRIMARY_TYPE
+		));
+		pw.println(ifNotFoundStr(true));
+		
+		
+		pw.println(String.format("\t\treturn new %s(HttpStatus.OK);", entityStr()));
 		pw.println("\t}\n");
 	}
 	
